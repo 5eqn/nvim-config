0c0e5ebea20eafc824344f4c5491c0670f597270
5eqn <subcat2077@gmail.com>
2023-03-31 16:14:19 +0800

    Use better filesystem tree

--- init.lua
+++ init.lua
@@ -8,6 +8,14 @@ require("telescope-conf")
 require('impatient')
 require('vimrc')
 
+-- idris2
+vim.api.nvim_create_autocmd("FileType", {
+  pattern = { "idris2" },
+  callback = function()
+    require('idris2').setup({})
+  end
+})
+
 -- note manager
 require('nothura')
 vim.keymap.set('n', ',n', ':lua GotoZathura()<CR>', { noremap = true, silent = true })
@@ -20,8 +28,8 @@ require("nvim-treesitter.configs").setup({
 })
 
 -- aerial
-vim.keymap.set('n', '}', '<cmd>AerialNext<CR>', { buffer = bufnr })
-vim.keymap.set('n', '{', '<cmd>AerialPrev<CR>', { buffer = bufnr })
+vim.keymap.set('n', '}', '<cmd>AerialNext<CR>')
+vim.keymap.set('n', '{', '<cmd>AerialPrev<CR>')
 
 -- harpoon
 vim.keymap.set('n', '<Space>t', function() require("harpoon.term").gotoTerminal(1) end, {})
